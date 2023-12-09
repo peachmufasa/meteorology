95763f469f4c9d4e441ff516bfd789cd222047e4
evgenius <mr.odnovil@mail.ru>
2023-12-09 14:05:28 +1000

    Added colors in tailwind, added font 'Roboto', width and height of html set to 100%

--- meteorology/tailwind.config.js
+++ meteorology/tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      'roboto': '"Roboto", Arial, sans-serif',
+    },
+    colors: {
+      'main-dark': '#141414',
+      'secondary-dark': '#1C1C1C',
+      'white': '#FFFFFF',
+      'perfect-blue': '#1A96BE',
+      'gray': '#A3A3A3',
+    },
     extend: {},
   },
   plugins: [],
