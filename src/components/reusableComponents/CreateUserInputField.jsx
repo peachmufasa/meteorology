51925e1badfeeb7b86c39da1eb7d821462cd8f4e
Alexandra Kochkar <alexandra-novoselova96@yandex.ru>
2024-01-21 22:31:21 +1000

    created section CreateUser and reusable components CreateUserInputField

---
+++ src/components/reusableComponents/CreateUserInputField.jsx
@@ -0,0 +1,10 @@
+import InputForm from "./InputForm.jsx";
+
+const CreateUserInputField = ({labelFor, labelContent ,inputId, inputType, maxLength}) => {
+    return <div className="flex flex-col gap-3 justify-center">
+        <label htmlFor={labelFor} className="px-6 text-2xl">{labelContent}</label>
+        <InputForm id={inputId} type={inputType} maxLength={maxLength} style="bg-dark-gray hover:bg-gray focus:bg-gray/50"/>
+    </div>
+}
+
+export default CreateUserInputField;
---
+++ src/components/sections/adminSections/CreateUserSection.jsx
@@ -0,0 +1,105 @@
+import SectionTitle from "../../reusableComponents/SectionTitle.jsx";
+import CreateUserInputField from "../../reusableComponents/CreateUserInputField.jsx";
+
+const CreateUserSection = () => {
+    return <div>
+        <header className="mb-12">
+            <SectionTitle titleText="Создать профиль"/>
+        </header>
+
+        <form>
+            <ul className="flex flex-wrap justify-evenly gap-4 mb-20">
+                <li className="w-[40%]">
+                    <CreateUserInputField
+                        inputId="name"
+                        inputType="text"
+                        labelContent="Имя"
+                        labelFor="name"/>
+                </li>
+                <li className="w-[40%]">
+                    <CreateUserInputField
+                        inputId="lastName"
+                        inputType="text"
+                        labelContent="Фамилия"
+                        labelFor="lastName"/>
+                </li>
+                <li className="w-[40%]">
+                    <CreateUserInputField
+                        inputId="middleName"
+                        inputType="text"
+                        labelContent="Отчество"
+                        labelFor="middleName"/>
+                </li>
+                <li className="w-[40%]">
+                    <CreateUserInputField
+                        inputId="phoneNumber"
+                        inputType="tel"
+                        labelContent="Телефон"
+                        labelFor="phoneNumber" maxLength={10}/>
+                </li>
+                <li className="w-[40%]">
+                    <CreateUserInputField
+                        inputId="postNumber"
+                        inputType="text"
+                        labelContent="Код поста"
+                        labelFor="postNumber"/>
+                </li>
+                <li className="w-[40%]">
+                    <CreateUserInputField
+                        inputId="login"
+                        inputType="text"
+                        labelContent="Логин"
+                        labelFor="login"/>
+                </li>
+                <li className="w-[40%]">
+                    <CreateUserInputField
+                        inputId="password"
+                        inputType="password"
+                        labelContent="Пароль"
+                        labelFor="password"/>
+                </li>
+                <li className="w-[40%]">
+                    <p className="mb-3 px-6 text-2xl">Роль</p>
+                    <div className="flex gap-7 justify-center">
+                        <div className="flex gap-3">
+                            <input
+                                className="w-6"
+                                type="radio" id="roleChoice1"
+                                name="role"
+                                value="admin"/>
+                            <label className="text-2xl" htmlFor="contactChoice1">Админ</label>
+                        </div>
+                        <div className="flex gap-3">
+                            <input
+                                className="w-6"
+                                type="radio"
+                                id="roleChoice2"
+                                name="role"
+                                value="sender"/>
+                            <label className="text-2xl" htmlFor="contactChoice2">Отправитель</label>
+                        </div>
+                        <div className="flex gap-3">
+                            <input
+                                className="w-6"
+                                type="radio"
+                                id="roleChoice3"
+                                name="role"
+                                value="hydrologist"/>
+                            <label className="text-2xl" htmlFor="contactChoice3">Гидролог</label>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div className="flex justify-center gap-28">
+                <button className="text-2xl text-red transition-all ease-in-out hover:text-red/50">
+                    Отменить
+                </button>
+                <button type="submit" className="py-5 px-10 text-2xl text-perfect-blue bg-dark-gray/50 rounded-full transition-all ease-in-out hover:bg-dark-gray active:bg-gray/30">
+                    Подтвердить
+                </button>
+            </div>
+        </form>
+    </div>
+}
+
+export default CreateUserSection;
