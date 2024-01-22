8072f695a646b9bda2ed1110164d57a1ecb6ab43
evgenius <mr.odnovil@mail.ru>
2024-01-22 20:23:01 +1000

    Deleted Link tag, edited input components, added onChange for input and onClick for login button

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,27 +1,52 @@
 import Logo from "../assets/images/Logo.svg";
-import ButtonMain from "./reusableComponents/ButtonMain.jsx";
-import InputForm from "./reusableComponents/InputForm.jsx";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {sections} from "../const/sections.jsx";
+import {useState} from "react";
+import useStore from "../store/store.js";
 
 
 const SignInForm = () => {
+
+  const logIn = useStore((state) => state.login)
+  const isAuth = useStore(state => state.isAuth)
+  const setAuth = useStore(state => state.setAuth)
+  setAuth(true)
+  const navigate = useNavigate()
+
+
+  // const logInAccount = (login, password) => {
+  //   // const response = logIn(login, password)
+  //
+  // }
+
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className=" bg-main-dark text-white flex flex-col min-h-screen justify-center">
       <div className="w-4/12 m-auto flex flex-col items-center">
         <img src={Logo} alt="Лого" className="h-[110px] w-[110px] mb-8" />
         <p className="text-center text-3xl mb-12">Авторизация</p>
         <form className="w-full flex flex-col items-center gap-8">
-          <InputForm type="text" placeholder="Логин" style="w-[50%]" />
-          <InputForm type="password" placeholder="Пароль" style="w-[50%]" />
-          <Link className='w-full flex justify-center' to={`/section/${Object.keys(sections)[0]}`}>
-            <ButtonMain style="w-[25%]">
+          <input onChange={e => setLogin(e.target.value)} type="text" placeholder="Логин" value={login} className='px-4 py-3 outline-none rounded-full border-2 border-transparent w-[50%]
+                            text-white text-lg placeholder:text-grey bg-dark-gray transition-all ease-in-out
+                            hover:border-perfect-blue focus:border-perfect-blue focus:shadow-perfect-blue -[#15A1CF] focus:shadow-[0px_0px_8px_perfect-blue]' />
+          <input onChange={e => setPassword(e.target.value)} type="password" placeholder="Пароль" value={password} className='px-4 py-3 outline-none rounded-full border-2 border-transparent w-[50%]
+                            text-white text-lg placeholder:text-grey bg-dark-gray transition-all ease-in-out
+                            hover:border-perfect-blue focus:border-perfect-blue focus:shadow-perfect-blue -[#15A1CF] focus:shadow-[0px_0px_8px_perfect-blue]' />
+        </form>
+        <button onClick={() => {
+            // logInAccount(login, password)
+            if (isAuth) {
+              navigate(`/section/${Object.keys(sections)[0]}`)
+            }
+          }
+        } className='px-4 py-3 text-white text-xl font-bold transition-all ease-in-out
+                              bg-perfect-blue outline-none rounded-full hover:bg-perfect-blue-hover active:bg-perfect-blue-active style="w-[25%]'>
               <span className='text-[1rem]'>
                 Войти
               </span>
-            </ButtonMain>
-          </Link>
-        </form>
+        </button>
       </div>
     </div>
   );
