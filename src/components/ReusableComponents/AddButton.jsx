edfbdc558f29f57045b67f700af527cb5b1e6ea1
Alexandra Kochkar <alexandra-novoselova96@yandex.ru>
2024-01-24 14:15:07 +1000

    disabled attribute and behaviour added

--- src/components/ReusableComponents/AddButton.jsx
+++ src/components/ReusableComponents/AddButton.jsx
@@ -1,8 +1,9 @@
-const AddButton = ({children, onClick}) => {
+const AddButton = ({children, onClick, disabled}) => {
     return <div className="w-[25%] flex justify-end">
         <button
-            className="flex gap-2 items-centerrounded-full py-3 px-5 transition-all ease-in-out hover:bg-dark-gray/60 active:bg-dark-gray"
-            onClick={onClick}>
+            className="flex gap-2 items-center rounded-full py-3 px-5 transition-all ease-in-out
+            hover:bg-dark-gray/60 active:bg-dark-gray disabled:hover:bg-transparent"
+            onClick={onClick} disabled={disabled && true}>
             <svg width="35px" height="35px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
