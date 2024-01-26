ed5a247a426e150e833f8db54f4e3834e3bd68c3
Alexandra Kochkar <alexandra-novoselova96@yandex.ru>
2024-01-26 16:25:59 +1000

    edited mb of the form

--- src/sections/SignInForm.jsx
+++ src/sections/SignInForm.jsx
@@ -20,7 +20,7 @@ const SignInForm = () => {
       <div className="w-4/12 m-auto flex flex-col items-center">
         <img src={Logo} alt="Лого" className="h-[110px] w-[110px] mb-8" />
         <p className="text-center text-3xl mb-12">Авторизация</p>
-        <form className="w-full flex flex-col items-center gap-8">
+        <form className="w-full flex flex-col items-center gap-8 mb-10">
           <input onChange={e => setLogin(e.target.value)} type="text" placeholder="Логин" value={login} className='px-4 py-3 outline-none rounded-full border-2 border-transparent w-[50%]
                             text-white text-lg placeholder:text-grey bg-dark-gray transition-all ease-in-out
                             hover:border-perfect-blue focus:border-perfect-blue focus:shadow-perfect-blue -[#15A1CF] focus:shadow-[0px_0px_8px_perfect-blue]' />
@@ -35,7 +35,7 @@ const SignInForm = () => {
           }
         } className='px-4 py-3 text-white text-xl font-bold transition-all ease-in-out
                               bg-perfect-blue outline-none rounded-full hover:bg-perfect-blue-hover active:bg-perfect-blue-active style="w-[25%]'>
-              <span className='text-[1rem]'>
+              <span className='text-[1rem] px-5'>
                 Войти
               </span>
         </button>
